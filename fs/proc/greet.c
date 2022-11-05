207828541ed20fa7bf7d9316a5f7d19c187282dc
Jinsu Park <dev.umijs@gmail.com>
2022-11-06 01:20:39 +0900

    add random in custom greet proc

--- fs/proc/greet.c
+++ fs/proc/greet.c
@@ -4,11 +4,17 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/utsname.h>
+#include <linux/random.h>
 
 static int greeting_message_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "Hello, world from Jinsu!\n");
-	return 0;
+    unsigned int random_int = get_random_int() % 10;
+    if ( 0 == (random_int % 2) ) {
+        seq_printf(m, "\"Hello\" from Jinsu!\n");
+    } else {
+        seq_printf(m, "\"Bye\" from Jinsu!\n");
+    }
+    return 0;
 }
 
 static int __init proc_greet_init(void)
